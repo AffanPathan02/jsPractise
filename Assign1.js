2af32cdd53bfafd386f204d817efefebd1b1f0e1
Affan_Pathan <91607963+AffanPathan02@users.noreply.github.com>
2022-09-19 22:58:33 +0530

    Add files via upload

---
+++ Assign1.js
@@ -0,0 +1,41 @@
+// function isLeapYear(year){
+//     if(year%4==0){
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// console.log(isLeapYear(2019))
+
+
+
+// function cToF(celsius) 
+// {
+//   var toFahren = celsius * 9 / 5 + 32;
+//    return toFahren
+// }
+
+// function fToC(fahrenheit) 
+// {
+//   var toCelc = (fahrenheit - 32) * 5 / 9;
+
+//     return toCelc
+// } 
+// console.log(fToC(101));
+
+
+
+
+// function factorial(n){
+//     let answer = 1;
+//     if (n == 0 || n == 1){
+//       return answer;
+//     }else{
+//       for(var i = n; i >= 1; i--){
+//         answer = answer * i;
+//       }
+//       return answer;
+//     }  
+//   }
+
+//   console.log(factorial(5))
